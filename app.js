f6176a6d7b0115d417175d08286c90e681f7b3aa
omkargundle <omkargundle1605@gmail.com>
2025-07-18 00:51:30 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,139 @@
+const { subscribe } = require("diagnostics_channel");
+const express = require("express")
+const app = express();
+const https = require("https");
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"));
+
+
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/",function(req, res){
+    var email= req.body.Email
+    var fname= req.body.Fname
+    var lname=req.body.Lname
+    const url= "https://us4.api.mailchimp.com/3.0/lists/ac860c934c"
+
+    const data =JSON.stringify({
+        members :[{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: fname,
+                LNAME: lname
+            }
+        }] 
+    })
+    const options ={
+        method: "POST",
+        auth :"OmkaSDr:6439a569dc00c101d90f75da7bd9d054-us4"
+    }
+
+    const request = https.request(url, options, function (response){
+         
+        var rawData= "";
+        response.on ("data", function(chunk){
+
+            rawData += chunk;
+        })
+        response.on("end", function(end){
+            try{
+                const fullyParsedData= JSON.parse(rawData);
+                if (response.statusCode=== 200){
+                    res.sendFile(__dirname + "/success.html");
+
+                }else{
+                    res.sendFile(__dirname +"/failure.html");
+                }
+                console.log(response.statusCode);
+                 
+            }catch(err){
+                console.log(err)
+                res.sendFile(__dirname +"/failure.html");
+            }
+        })
+    })
+request.write(data);
+request.end();
+
+})
+
+app.post("/failure", function(req,res){
+    res.redirect("/");
+})
+
+
+app.listen(process.env.PORT ||3000, function (req, res) {
+    console.log("Serve is listening at port 3000");
+})
+
+
+// audienc e id
+// ac860c934c
+
+// apikey
+// 6439a569dc00c101d90f75da7bd9d054-us4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.post("/", function (req, res) {
+//     var fname = req.body.Fname;
+//     var lname = req.body.Lname;
+//     var email = req.body.Email;
+//     const url = "https://us4.api.mailchimp.com/3.0/lists/ac860c934c"
+
+//     const data = JSON.stringify({
+//         members: [
+//             {
+//                 email_address: email,
+//                 status: "subscribed",
+//                 merge_fields: {
+//                     FNAME: fname,
+//                     LNAME: lname
+//                 }
+//             }
+//     ]
+
+    
+//     })
+
+// const options = {
+//     method: "POST",
+//     auth: "Omkar:d71c4e293419aef0eda5f39a1eb9cfef-us4",
+// }
+// const request = https.request(url, options, function (response) {
+//     response.on("data", function (data) {
+//         var returnData = JSON.parse(data);
+//         console.log(returnData);
+
+//     })
+// })
+// request.write(data);
+// request.end();
+  
+// })
